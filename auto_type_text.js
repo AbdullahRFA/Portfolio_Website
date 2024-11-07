bf85352d2296c27881befb3c6f6d3e2e813a30d7
AbdullahRFA <shakibrybmn@gmail.com>
2024-11-07 16:40:50 +0600

    added IOT expert in autu typing section

--- auto_type_text.js
+++ auto_type_text.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#degination', {
-  strings: ['Programer', 'Web Developer', 'UX-UI Designer'],
+  strings: ['Programer', 'Web Developer', 'UX-UI Designer','IOT Expert'],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true // Set this to true for infinite looping
